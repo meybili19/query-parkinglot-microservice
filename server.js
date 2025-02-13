93c89db183b83035ed0d2e40c4603b4b7df6df7b
Meybili <109104859+meybili19@users.noreply.github.com>
2025-02-12 21:24:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./src/app');
-const PORT = process.env.PORT || 6001;
+const PORT = process.env.PORT || 7003;
 
 app.listen(PORT, () => {
     console.log(`🚀 Server running at http://localhost:${PORT}`);
